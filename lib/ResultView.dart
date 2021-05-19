d2a1d25ed45b332818bb9f665a11ddb019e5dfda
DESKTOP-ODFODDM\ADmin <ldyjka@gmail.com>
2021-05-19 17:37:05 +0900

    resultview, mapview google map 적용, api키 공용키로 변경

--- lib/ResultView.dart
+++ lib/ResultView.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:get/get.dart';
 import 'package:get/get_navigation/get_navigation.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class ResultView extends StatefulWidget {
   @override
@@ -14,60 +15,80 @@ class _ResultViewState extends State<ResultView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        floatingActionButton:
-        Container(height:300,
-          child: Column(mainAxisAlignment: MainAxisAlignment.spaceBetween,crossAxisAlignment: CrossAxisAlignment.end,
+        floatingActionButton: Container(
+          height: 300,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.end,
             children: [
-              FloatingActionButton.extended(backgroundColor:Colors.white,heroTag: "Button1",
-                icon: Icon( Icons.home, color: Theme.of(context).accentColor,),
-                label: Text("홈으로", style: TextStyle(color: Theme.of(context).accentColor),),
-                onPressed: (){
+              FloatingActionButton.extended(
+                backgroundColor: Colors.white,
+                heroTag: "Button1",
+                icon: Icon(
+                  Icons.home,
+                  color: Theme.of(context).accentColor,
+                ),
+                label: Text(
+                  "홈으로",
+                  style: TextStyle(color: Theme.of(context).accentColor),
+                ),
+                onPressed: () {
                   Get.offAll(MyHomePage(), transition: Transition.fadeIn);
                 },
               ),
-              FloatingActionButton.extended(heroTag: "Button2",
-                icon: Icon( Icons.restaurant_menu, color: Colors.white,),
-                label: Text("다시뽑기!", style: TextStyle(color: Colors.white),),
-
-              ),
-              Card(child:
-                Container(width: MediaQuery.of(context).size.width - 60,height: 150, color: Colors.white,margin: EdgeInsets.all(10),
-
-
+              FloatingActionButton.extended(
+                heroTag: "Button2",
+                icon: Icon(
+                  Icons.restaurant_menu,
+                  color: Colors.white,
                 ),
-                shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(10))),
-
+                label: Text(
+                  "다시뽑기!",
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+              Card(
+                child: Container(
+                  width: MediaQuery.of(context).size.width - 60,
+                  height: 150,
+                  color: Colors.white,
+                  margin: EdgeInsets.all(10),
+                ),
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(10))),
               )
             ],
           ),
         ),
         appBar: AppBar(),
-        body:Stack(
+        body: Stack(
           children: [
-            Container(height: MediaQuery.of(context).size.height,
-              child: CachedNetworkImage(fit: BoxFit.fitHeight,
-                imageUrl: "https://blog.kakaocdn.net/dn/cyXq4J/btqB2v48gwJ/skJQBJUdvTuPmKYqBM6rbK/img.png",
+            Container(
+              height: MediaQuery.of(context).size.height,
+              child: GoogleMap(
+                mapType: MapType.normal,
+                myLocationEnabled: true,
+                initialCameraPosition:
+                    CameraPosition(target: LatLng(40, 40), zoom: 14),
               ),
             ),
-
-
-
             Container(
-                height: MediaQuery.of(context).size.height,width: MediaQuery.of(context).size.width,
-                child: Column(mainAxisAlignment: MainAxisAlignment.spaceEvenly,crossAxisAlignment: CrossAxisAlignment.center,
+                height: MediaQuery.of(context).size.height,
+                width: MediaQuery.of(context).size.width,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
-
                     Container(
-                        width: 200,height: 200,
-                        decoration: BoxDecoration(shape: BoxShape.circle,color: Color(0x99ffffff)),
-                        child:  Icon(Icons.location_on, size: 50,color: Colors.amber)),
-
-
+                        width: 200,
+                        height: 200,
+                        decoration: BoxDecoration(
+                            shape: BoxShape.circle, color: Color(0x99ffffff)),
+                        child: Icon(Icons.location_on,
+                            size: 50, color: Colors.amber)),
                   ],
-                )
-            ),
+                )),
           ],
-        )
-    );
+        ));
   }
 }
